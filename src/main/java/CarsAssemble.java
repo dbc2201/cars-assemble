008ca493ed91d975196b80e152a1edeee44bbdf8
dbc2201 <divyanshb22@gmail.com>
2022-02-21 17:59:50 +0530

    create a method to calculate success rate based on the speed of the assembly line

--- src/main/java/CarsAssemble.java
+++ src/main/java/CarsAssemble.java
@@ -1,5 +1,17 @@
 public class CarsAssemble {
 
+    private double getSuccessRate(int speed) {
+        if (speed >= 1 && speed <= 4) {
+            return 1.0;
+        } else if (speed >= 5 && speed <= 8) {
+            return 0.9;
+        } else if (speed == 9) {
+            return 0.8;
+        } else if (speed == 10) {
+            return 0.77;
+        } else return Integer.MIN_VALUE;
+    }
+
     public double productionRatePerHour(int speed) {
         throw new UnsupportedOperationException("Please implement the AssemblyLine.productionRateperHour() method");
     }
